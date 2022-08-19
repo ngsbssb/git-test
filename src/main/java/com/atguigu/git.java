7a31740ef6aaac95d00b7c44708cec41c08e1d2d
郑嘉颖 <2664854040@qq.com>
2022-08-19 08:13:10 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -5,5 +5,6 @@ public class git {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git");
+        System.out.println("Hello hot-fix");
     }
 }
